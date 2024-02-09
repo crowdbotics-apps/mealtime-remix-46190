4fbd4a45177b981fb1782f4350a33030e2554c9d
Crowdbotics <team@crowdbotics.com>
2024-02-09 19:40:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mealtime-remix-46190",
+  url: "https://mealtime-remix-46190.botics.co",
+  api: "https://mealtime-remix-46190.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
